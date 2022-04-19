22b9a03837a99c8593ea7372de93e037f22c0d27
Jdjacob3 <53888899+Jdjacob3@users.noreply.github.com>
2022-04-19 16:14:35 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,9 +7,16 @@ Thread nineDoFSensor;
 Thread motorController;
 Thread dataReporting;
 
-#define ERROR_CONVERSION .000040
-#define DECEL_CAP 0.005
-#define ACCEL_CAP 0.005
+#define ERROR_CONVERSION_CONSTANT 0.0005
+#define p (ERROR_CONVERSION_CONSTANT / refreshn)                 //p term (error conversion, error gets multiplied by this then added to currspeed)
+//#define DECEL_CAP 0.005
+//#define ACCEL_CAP 0.005
+#define mint 0.07                   //abs value of minimum stable throttle (7%)
+//#define med_gyro 420
+//#define small_gyro 210
+#define refreshn 1 / 100 
+#define refresh 100ms
+#define ming 300
 
 
 void getGyroData();
@@ -28,13 +35,6 @@ float headings [3]; //The basic data from the sensor (heading, roll, pitch)
 float gyros[3]; //Array for the readouts of the angular velocities from the sensor in the x y and z direction
 float currSpeed; //VAriable used to send the requested speed to the motor and report the current speed to the command module
 float prevSpeed; //Variable used to calculate the new speed based on the change in error (angular velocity)
-
-
-
-
-
-
-
 
 
 
@@ -62,16 +62,21 @@ int main() {
   
   
   Timer s;
-  
-  /*
+ 
+
+ 
   s.start();
   while (s.elapsed_time() < 10s)
   {
-    motor.throttle_3d(.1);
+    motor.throttle_3d(.07);
   }
   s.reset(); 
-  */
 
+  while (s.elapsed_time() < 10s)
+  {
+    motor.throttle_3d(.0);
+  }
+  s.reset(); 
 
   //Starting the threads
     nineDoFSensor.start(getGyroData);
@@ -79,22 +84,15 @@ int main() {
     dataReporting.start(reportData);
 
   
-printf("howdy\n");
 
   s.start(); 
 
-printf("cable\n");
-
   while(1) {
-
-    printf("finger\n");
-
-    if (s.elapsed_time() > 120s)
+    if (s.elapsed_time() > 10s)
     {
       nineDoFSensor.terminate();
       motorController.terminate();
       dataReporting.terminate();
-      printf("almost\n");
     }
   }
     printf("done\n");
@@ -125,37 +123,42 @@ void setSpeed(){
   Timer t;
   while (true) {
     t.start();
+
+    float throttleChange = gyros[1] * p; 
+
+
     //converting the most recent angular velocity to speed
     //currSpeed = gyros[1] * ANGULAR_VELOCITY_TO_SPEED;
 
-  /*
-    if (gyros[1] > 0) {
-      currSpeed -= (gyros[1] * ANGULAR_VELOCITY_TO_SPEED);      
-    }
+/*
+    if (gyros[1] > 0) {                                             //V1 
+      currSpeed -= (gyros[1] * ANGULAR_VELOCITY_TO_SPEED);          //V1
+    }                                                               //V1
     else if (gyros[1] < 0) {                                        //V1
-      currSpeed += (gyros[1] * ANGULAR_VELOCITY_TO_SPEED);
+      currSpeed += (gyros[1] * ANGULAR_VELOCITY_TO_SPEED);          //V1
     }
-  */
+*/
 
     //currSpeed = prevSpeed + (gyros[1] * ERROR_CONVERSION);
 
-    /*
-    if ((currSpeed < .03) && (currSpeed >= .01 )) {
-      currSpeed = .03; 
-    }
-    else if ((currSpeed < .01) && (currSpeed > -.01)) {
+/*
+    if ((currSpeed < .03) && (currSpeed >= .01 )) {               //V2
+      currSpeed = .03;                                            //V2
+    }                                                             //V2
+    else if ((currSpeed < .01) && (currSpeed > -.01)) {           //V2
       currSpeed = 0;                                              //V2 
-    }
-    else if ((currSpeed <= -.01) && (currSpeed > -.03)) {
-      currSpeed = -.03;
-    }
-    */
+    }                                                             //V2
+    else if ((currSpeed <= -.01) && (currSpeed > -.03)) {         //V2
+      currSpeed = -.03;                                           //V2
+    }                                                             //V2
+*/
 
-    float throttleChange = gyros[1] * ERROR_CONVERSION; 
+
 
 
 //checks and adjusts the throttle change if it is over the decel or accel caps. 
 //Decel is defined as reducing the absolute value of currSpeed, which is just coasting. it is not powered or controlled
+/*
     if (currSpeed == 0 ) {                  //absolute acceleration is always acceleration when starting from 0 rpm; you can't decelerate from 0
       if (throttleChange > ACCEL_CAP) {     //checking if outside positive cap
         throttleChange = ACCEL_CAP;
@@ -180,32 +183,86 @@ void setSpeed(){
         throttleChange = DECEL_CAP;
         }
     }
+*/
 
+
+
+
+//Low throttle adjustment 
+/*
+//VERSION 1
+if (mint > currSpeed && currSpeed > -mint) {                  //if currSpeed is in low range, we should not directly use the P function     
+    
+    if(gyros[1] > med_gyro) {
+      currSpeed = mint + 0.005;
+      while (t.elapsed_time() < refresh + 3000ms){
+      motor.throttle_3d(currSpeed);}
+    }
+    else if (med_gyro > gyros[1] && gyros[1] > small_gyro) {                //min throttle
+      while (t.elapsed_time() < refresh + 3000ms){                          //V1
+      motor.throttle_3d(mint+0.005);} 
+    }
+    else if (small_gyro > gyros[1] && gyros[1] > -small_gyro) {
+      while (t.elapsed_time() < refresh + 3000ms){
+      motor.throttle_3d(0);}
+      currSpeed = 0; 
+    }
+    else if (-small_gyro > gyros[1] && gyros[1] > -med_gyro) {
+      while (t.elapsed_time() < refresh + 3000ms){
+      motor.throttle_3d(-mint -0.005);}
+    }
+    else if (-med_gyro > gyros[1]) {
+      while (t.elapsed_time() < refresh + 3000ms){
+      currSpeed = (-mint -0.005);}
+      
+    }
+}
 
 //changes the currspeed based on the error it should correct
-  currSpeed = prevSpeed + throttleChange;
-
-//if (-0.05 < currSpeed && currSpeed < 0.05){
-//  while (t.elapsed_time() < 700ms)    //if the flywheel is between -5 and 5% throttle, set the refresh period to 0.7s (1.4 hz)
-//    {                                 //so that the gyro isn't trying to interpret the short but weird actions of the motor in this throttle range.
-//      //Send the speed to the motor controller
-//      motor.throttle_3d(currSpeed); 
-//    }
-//}
-
-//else {
-    while (t.elapsed_time() < 200ms)   //the refresh frequency is 1000/52 or about 19.2 Hertz
-    {
-      //Send the speed to the motor controller
-      motor.throttle_3d(currSpeed); 
+else if(currSpeed >= mint || -mint >= currSpeed) {
+      currSpeed = prevSpeed + throttleChange;
+      while (t.elapsed_time() < refresh){  
+      motor.throttle_3d(currSpeed);    
     }
-  //}
+}
+*/
+
+
+
+
+if (mint < abs(currSpeed)){
+  currSpeed = prevSpeed + throttleChange;
+}
+
+else if (abs(gyros[1]) < ming) {
+//don't do anything becuase our error is acceptable
+}
+
+else if (ming < gyros[1] && abs(currSpeed) < 0.01) {
+  currSpeed = -mint;
+}
+
+else if (gyros[1] < -ming && abs(currSpeed) < 0.01) {
+  currSpeed = mint;
+}
+
+else if (ming < gyros[1] && (currSpeed < (mint + 0.01)) && (currSpeed > (mint - 0.01)) ) {
+  currSpeed = 0;
+}
+
+else if (-ming > gyros[1] && (currSpeed < (-mint + 0.01)) && (currSpeed > (-mint - 0.01)) ) {
+  currSpeed = 0;
+}
+
+
+while (t.elapsed_time() < refresh){  
+      motor.throttle_3d(currSpeed);
+
     //ThisThread::sleep_for(5ms); 
     t.reset(); 
 
     prevSpeed = currSpeed; 
 
-    printf("about");
   }
 }
 
@@ -220,6 +277,6 @@ void reportData(){
 
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 
-    ThisThread::sleep_for(2s); 
+    ThisThread::sleep_for(1s); 
   }
 }
